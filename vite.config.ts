333a619cce46d96c8b82fe19369579a54806bedc
Freewind <nowindlee@gmail.com>
2024-12-19 16:07:05 +0800

    do not enable in test

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import functionLogger from './plugins/vite-plugin-function-logger'
 
 export default defineConfig({
   plugins: [
-    react(),
+    react() as any,
     functionLogger({
-      pattern: /^test/ // 只记录以test开头的函数
+      pattern: /^test/, // 只记录以test开头的函数
+      enabled: process.env.NODE_ENV !== 'test' // 在测试环境下禁用插件
     })
   ],
 })
